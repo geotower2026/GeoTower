29a5c4c73a78f0eecc8cec3e029970506c6d5418
Your Name <Josinei@GEOLOGNB24.geomaritima.local>
2026-02-22 10:59:34 -0400

    Remover flex-1 do layout para eliminar espaço em branco desnecessário

--- frontend/src/components/AppLayout.js
+++ frontend/src/components/AppLayout.js
@@ -6,8 +6,8 @@ const AppLayout = ({ children }) => {
     <div className="h-[100svh] w-full overflow-hidden bg-[#f7f7fb] flex flex-col">
       <Header />
 
-      {/* Scrollable content area */}
-      <main className="flex-1 overflow-y-auto overscroll-none">
+      {/* Scrollable content area - takes only needed height */}
+      <main className="overflow-y-auto overscroll-none">
         {children}
       </main>
     </div>
